cbc2643dd98cc2a6702cb863ed1e2e12fd1d4947
Mukul Bedi <mukulbedi07@gmail.com>
2024-08-07 00:32:11 +0530

    Connected Auth with React with Cookie

--- routes/Users.js
+++ routes/Users.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const { fetchUserById, updateUser } = require('../controller/User');
 
 const router = express.Router();
 //  /users is already added in base path
-router.get('/:id', fetchUserById)
+router.get('/own', fetchUserById)
       .patch('/:id', updateUser)
 
 exports.router = router;
